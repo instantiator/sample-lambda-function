00ac039eb7c91df79a34cbf6fb7feace9516f3e2
Lewis Westbury <lewis@cantab.net>
2021-04-20 20:48:38 +0100

    Add support for writing to sample S3 bucket

--- src/SampleLambdaFunction/Function.cs
+++ src/SampleLambdaFunction/Function.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-
+using Amazon;
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -30,8 +33,40 @@ namespace SampleLambdaFunction
             using (var http = new HttpClient())
             {
                 var output = await http.GetStringAsync(query);
-                return output;
+                var bucket = "sample-lambda-storage";
+                var key = $"weather_{DateTime.Now.Ticks}.json";
+                var stored = await PutToS3Async(bucket, key, output, context);
+                var msg = stored
+                    ? $"Successfully stored weather data: {bucket}:{key}"
+                    : $"Failed to store weather data.";
+                return msg;
             }
+        }
+
+        private async Task<bool> PutToS3Async(string bucket, string key, string content, ILambdaContext context)
+        {
+            var request = new PutObjectRequest
+            {
+                BucketName = bucket,
+                Key = key,
+                ContentBody = content
+            };
+
+            using (var s3 = new AmazonS3Client(RegionEndpoint.EUWest2))
+            {
+                var response = await s3.PutObjectAsync(request);
+                if (response.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                else
+                {
+                    context.Logger.Log($"{response.HttpStatusCode} encountered putting: {bucket}:{key}");
+                    return false;
+                }
+
+            }
+
         }
     }
 }
